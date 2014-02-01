9af8aa4d30273f3bc9e6df2332111364fc16fc94
pdostal <git@pdostal.cz>
2014-02-01 12:59:52 +0100

    Repair month problem

--- mail2db.php
+++ mail2db.php
@@ -1,5 +1,6 @@
 <?php
 	require_once('config.php');
+	$monthnames = array('Jan' => '01', 'Feb' => '02');
 	if ( !empty($config['server']) && !empty($config['username']) && !empty($config['password']) && !empty($config['database']) ) {
 		$mysqli = new mysqli($config['server'], $config['username'], $config['password'], $config['database']);
 	}
@@ -11,8 +12,11 @@
 	$parse = file('/var/log/mail.log');
 	foreach ($parse as $line) {
 		if (preg_match('/postfix\/qmgr\[.+|postfix\/smtp\[.+/i', $line)) {
-			$datetime = trim(preg_replace('/ [a-zA-Z0-9]+ postfix\/.+$/i', '', $line));
-			$datetime = date('Y-m-').preg_replace('/^([A-Z][a-z]+) ([0-9]+) (.+)/i', '$2 $3', $datetime);
+			$dateyear = date('Y-');
+			$datemonth = $monthnames[trim(preg_replace('/^([A-Z][a-z]+) .+/i', '$1', $line))];
+			$dateday = trim(preg_replace('/^[a-zA\-Z]+ +([0-9]+) .+/i', '-$1', $line));
+			$datetime = trim(preg_replace('/^[a-zA\-Z]+ +[0-9]+ ([0-9]+:[0-9]+:[0-9]+) .+/i', ' $1', $line));
+			$date = trim($dateyear.$datemonth.$dateday.' '.$datetime);
 			$type = trim(preg_replace('/(^.+postfix\/)([a-z]+)(.+$)/i', '$2', $line));
 		    if ($type !== 'qmgr') {
 		    	$num = trim(preg_replace('/(^.+postfix\/[a-z]+\[)([0-9]+)(\]: .+$)/i', '$2', $line));
@@ -21,15 +25,16 @@
 		    }
 			$msg = preg_replace('/^.+postfix\/[a-z]+\[[0-9]+\]: /i', '', $line);
 			$msg = ucfirst(trim(htmlentities(preg_replace('/^[A-Z0-9]+: /i', '', $msg))));
-			//echo trim($line)."\n".$datetime.' X '.md5($msg).' X '.$type.' X '.$num.' X '.$msg."\n\n\n";
+			echo trim($line)."\n".$date.' X '.md5($msg).' X '.$type.' X '.$num.' X '.$msg."\n\n\n";
 			$duplicate = false;
 			foreach ($db as $field) {
-				if ($field['datetime'] == $datetime && md5($field['msg']) == md5($msg)) {
+				if ($field['datetime'] == $date && md5($field['msg']) == md5($msg)) {
 					$duplicate = true;
 				}
 			}
+			//$duplicate = true;
 			if ($duplicate == false) {
-				if (!$mysqli->query("INSERT INTO `".$config['database']."`.`".$config['table']."` (`datetime`, `type`, `num`, `msg`) VALUES ('".$datetime."', '".$type."', '".$num."', '".$msg."');")) {
+				if (!$mysqli->query("INSERT INTO `".$config['database']."`.`".$config['table']."` (`datetime`, `type`, `num`, `msg`) VALUES ('".$date."', '".$type."', '".$num."', '".$msg."');")) {
 					printf("%s\n", $mysqli->error);
 				}
 			}
